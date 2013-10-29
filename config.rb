27e6c248da55e4077e317342194a72e5c33ab517
rpavlov <arepavlov@gmail.com>
2013-10-29 14:13:56 -0400

    Styling

--- config.rb
+++ config.rb
@@ -4,12 +4,14 @@
 
 # Time.zone = "UTC"
 
+
+
 activate :blog do |blog|
-  # blog.prefix = "blog"
+  #blog.prefix = "blog"
   # blog.permalink = ":year/:month/:day/:title.html"
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
-  # blog.layout = "layout"
+  blog.layout = "layouts/article_layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = ":year.html"
@@ -25,9 +27,11 @@ activate :blog do |blog|
   # blog.page_link = "page/:num"
 end
 
+activate :directory_indexes
+
 page "/feed.xml", :layout => false
 
-### 
+###
 # Compass
 ###
 
@@ -45,13 +49,13 @@ page "/feed.xml", :layout => false
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -86,21 +90,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
