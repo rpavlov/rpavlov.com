e5db0429f6afc5dc595d1fd5c9b59dcac3289cfb
rpavlov <roumen@polymorphic.ca>
2016-06-29 11:37:49 -0400

    Fix Links

--- config.rb
+++ config.rb
@@ -105,7 +105,7 @@ configure :build do
   # Use relative URLs
   activate :relative_assets
   set :relative_links, true
-
+  set :http_prefix, '/rpavlov.com'
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
