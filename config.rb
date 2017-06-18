6ae33641c1536b04460c97660ed6fba19fe15f78
rpavlov <roumen@polymorphic.ca>
2017-06-18 13:50:08 +0200

    Pagination

--- config.rb
+++ config.rb
@@ -22,7 +22,7 @@ activate :blog do |blog|
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
 
-  # blog.paginate = true
+  blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/:num"
 end
